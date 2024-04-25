8f88823929f3e2d7aae9ef841c5e80a11756ea72
mustafayazar <mustafa.yazar@viravira.co>
2024-04-25 21:34:55 +0300

    Update Swagger documentation

--- src/libs/swagger/index.ts
+++ src/libs/swagger/index.ts
@@ -4,13 +4,18 @@ export const useSwagger = () =>
   swagger({
     documentation: {
       info: {
-        title: "Elysia",
-        description: "Elysia API Documentation",
+        title: "X(Twitter) Clone API Documentation with ElysiaJS",
+        description: "API Documentation for X(Twitter) Clone with ElysiaJS",
         version: "1.0.0",
       },
       tags: [
         { name: "App", description: "General endpoints" },
         { name: "Auth", description: "Authentication endpoints" },
+        { name: "Account", description: "Account endpoints" },
+        { name: "User", description: "User endpoints" },
+        { name: "Tweet", description: "Tweet endpoints" },
+        { name: "Follow", description: "Follow endpoints" },
+        { name: "Timeline", description: "Timeline endpoints" },
       ],
 
       components: {
